2d3fbfa137b81aa3664de1c12456633c1421a5b2
zakymuhammad <arzaki666@gmail.com>
2023-03-21 09:00:48 +0700

    mengambil data dari API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
+/* eslint-disable jsx-a11y/alt-text */
 import "./index.css";
 import { useEffect, useState } from "react";
 import { getMovieList, searchMovie } from "./api";
 
 const App = () => {
+  const [popularMovies, setPopularMovies] = useState([]);
+
   useEffect(() => {
-    getMovieList();
+    getMovieList().then((result) => {
+      setPopularMovies(result);
+    });
   }, []);
 
-  const [count, setCount] = useState(0);
-  const search = (q) => {
-    console.log({ q });
+  const PopularMovieList = () => {
+    return popularMovies.map((movie, i) => {
+      return (
+        <div className="movie-wrapper" key={i}>
+          <div className="movie-title">{movie.title}</div>
+          <img
+            className="movie-image"
+            src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}
+          />
+          <div className="movie-date">{movie.release_date}</div>
+          <div className="movie-rate">{movie.vote_average}</div>
+        </div>
+      );
+    });
   };
+
+  const search = async (q) => {
+    if (q.length > 3) {
+      const query = await searchMovie(q);
+      setPopularMovies(query.results);
+    }
+  };
+
+  console.log({ popularMovies: popularMovies });
 
   return (
     <div className="App">
@@ -24,12 +49,7 @@ const App = () => {
           onChange={({ target }) => search(target.value)}
         />
         <div className="movie-container">
-          <div className="movie-wrapper">
-            <div className="movie-title">Contoh 1</div>
-            <img className="movie-image" src="" />
-            <div className="movie-date">23-12-1233</div>
-            <div className="movie-rate">7.6</div>
-          </div>
+          <PopularMovieList />
         </div>
       </header>
     </div>
--- src/api.js
+++ src/api.js
@@ -5,10 +5,12 @@ const baseUrl = process.env.REACT_APP_BASEURL;
 
 export const getMovieList = async () => {
   const movie = await axios.get(`${baseUrl}/movie/popular?api_key=${apiKey}`);
-  console.log({ movieList: movie });
+  return movie.data.results;
 };
 
 export const searchMovie = async (q) => {
-  const search = await axios.get(q);
-  return;
+  const search = await axios.get(
+    `${baseUrl}/search/movie?query=${q}&api_key=${apiKey}`
+  );
+  return search.data;
 };
