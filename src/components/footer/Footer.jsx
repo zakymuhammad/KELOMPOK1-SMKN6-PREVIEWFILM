db8438b83e51d3240cff1be851d34c65d1ebc5c8
RPL14 SMKN6 <agnescesarsari@gmail.com>
2023-04-17 12:39:53 +0700

    edit logo footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,7 +5,7 @@ import './footer.scss';
 import { Link } from 'react-router-dom';
 
 import bg from '../../assets/footer-bg.jpg';
-import logo from '../../assets/tmovie.png';
+import logo from '../../assets/tmoview.png';
 
 const Footer = () => {
   return (
