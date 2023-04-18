305cda585594db348ad8d6633ec8b13509f6c75e
maisyafaradilla <maisyafaradilla646@gmail.com>
2023-04-18 08:37:37 +0700

    menambahkan catalog

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
+import { useParams } from 'react-router';
+
 const Catalog = () => {
+
+const { category } = useParams();
+
+console.log(category);
+
   return <div>Catalog</div>;
 };
 
