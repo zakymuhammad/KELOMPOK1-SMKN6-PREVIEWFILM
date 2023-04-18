639b325a9beed73685352c39e128f4f3a75d35a6
zaskiainayah <zaskiainayah06@gmail.com>
2023-04-18 09:55:42 +0700

    mengedit catalog

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -1,14 +1,27 @@
-import React from "react";
+import React from 'react';
 
 import { useParams } from 'react-router';
 
+import PageHeader from '../components/page-header/PageHeader';
+
+import { category as cate } from '../api/tmdbApi';
+
 const Catalog = () => {
 
-const { category } = useParams();
+  const { category } = useParams();
 
-console.log(category);
+  console.log(category);
 
-  return <div>Catalog</div>;
-};
+    return ( 
+    <>
+        <PageHeader>
+          {category == cate.movie ? 'Movies' : 'TV Series'}
+        </PageHeader>
+    </>
+
+
+    );
+
+}
 
 export default Catalog;
