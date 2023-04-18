dadb3b9273d4f43a68982861a2116aa2576e169f
RPL14 SMKN6 <agnescesarsari@gmail.com>
2023-04-18 11:41:18 +0700

    mengedit detail pages

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router";
 import tmdbApi from "../../api/tmdbApi";
 import apiConfig from "../../api/apiConfig";
 
+import './detail.scss';
 
 const Detail = () => {
 
@@ -24,7 +25,32 @@ const Detail = () => {
     <>
       {
         item && (
-          <div className="banner" style={{backgroundImage: 'url($(apiConfig.originalImage(item.backdrop_path || item.poster_path)})'}}></div>
+          <>
+            <div className="banner" style={{backgroundImage: 'url($(apiConfig.originalImage(item.backdrop_path || item.poster_path)})'}}></div>
+            <div className="container mb-3 movie-content">
+              <div className="movie-content__poster">
+                <div className="movie-content__poster__img" style={{backgroundImage: 'url($(apiConfig.originalImage(item.poster_path || item.backdrop_path)})'}}></div>
+              </div>
+              <div className="movie-content__info">
+                  <h1 className="title">
+                    {item.title || item.name}
+                  </h1>
+                  <div className="genres">
+                    {
+                      item.genres && item.genres.slice(0, 5).map((genre, i) => (
+                      <span key={i} className="genres__item">{genre.name}</span>))
+                    }
+                  </div>
+                  <p className="overview">{item.overview}</p>
+                  <div className="cast">
+                    <div className="section__header">
+                      <h2>Casts</h2>
+                    </div>
+                    {/* casts list */}
+                  </div>
+              </div>
+            </div>
+          </>
         )
       }
     </> 
