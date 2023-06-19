b86370149c452374b1ebc18d40c797105943e929
zakymuhammad <arzaki666@gmail.com>
2023-06-19 09:01:07 +0700

    fix bug router

--- src/components/login/Signin.jsx
+++ src/components/login/Signin.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import { useHistory, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const Signin = () => {
   const navigate = useNavigate();
